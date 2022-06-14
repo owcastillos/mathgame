cdcf9ccf5ce5d6cd2366f707ec8e70ad20d7db18
Otto Castillo <103940843+ottoCastilloADL@users.noreply.github.com>
2022-06-14 15:02:19 -0500

    :wrench: Fixing substract operation

--- src/app/services/math/math.service.ts
+++ src/app/services/math/math.service.ts
@@ -11,15 +11,18 @@ export class MathService {
 
   getExpressions(): any {
     const exp = [];
-    for (let i = 0; i < MathService.SIZE; i++) {
-      exp.push(this.getExpression(i % 2 == 0))
+    while (exp.length < MathService.SIZE) {
+      const localExp = this.getExpression(exp.length % 2 == 0)
+      if (exp.indexOf(localExp) < 0) {
+        exp.push(localExp)
+      }
     }
     return exp;
   }
 
   private getExpression(adding: boolean): string {
-    const t0 = Math.floor(Math.random() * 20)
-    const t1 = Math.floor(Math.random() * (adding ? (t0 > 10 ? 20 : 10) - t0 : Math.min(t0, 10)))
+    const t0 = Math.floor(Math.random() * 19) + 1
+    const t1 = Math.floor(Math.random() * (adding ? 20 - t0 : Math.min(t0, 10)))
     return `${t0}${adding ? '+' : '-'}${Math.max(t1, 1)}`
   }
 }
